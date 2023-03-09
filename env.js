4e3382193215d4614f18a056f49805190983ce1f
KailasMahavarkar <kailashmahavarkar5@gmail.com>
2023-03-09 21:36:01 +0530

    (ignore) test

--- env.js
+++
@@ -1,8 +0,0 @@
-const dotenv = require("dotenv");
-dotenv.config();
-
-const env = {
-	DB_URL: process.env.DB_URL,
-};
-
-module.exports = env;
--- router.js
+++ router.js
@@ -3,20 +3,20 @@ const router = express.Router();
 
 // import controllers
 const readTodo = require("./controllers/readTodo");
-const createTodo = require("./controllers/createTodo");
-const deleteTodo = require("./controllers/deleteTodo");
-const updateTodo = require("./controllers/updateTodo");
+// const createTodo = require("./controllers/createTodo");
+// const deleteTodo = require("./controllers/deleteTodo");
+// const updateTodo = require("./controllers/updateTodo");
 
 // read single todo by uuid
 router.get("/:uuid", readTodo);
 
 // create single todo by uuid
-router.post("/", createTodo);
+// router.post("/", createTodo);
 
-// // update single todo by uuid
-router.patch("/:uuid", updateTodo);
+// // // update single todo by uuid
+// router.patch("/:uuid", updateTodo);
 
-// // delete single todo by uuid
-router.delete("/:uuid/:taskId", deleteTodo);
+// // // delete single todo by uuid
+// router.delete("/:uuid/:taskId", deleteTodo);
 
 module.exports = router;
